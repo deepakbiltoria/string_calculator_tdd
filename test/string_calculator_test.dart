dd0880156165f42bdd4c50f574d162323fb4fa05
deepakbiltoria <deepakbiltoria@gmail.com>
2025-02-25 14:50:24 +0530

    grouped String tests

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,12 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:string_calculator_tdd/string_calculator.dart';
 
 void main(){
-   test('empty string test', (){
-     final val = Calculator();
-     expect(val.add(""), 0);
-   });
-   test('try single number string', () {
-     final calculator = Calculator();
-     expect(calculator.add("1"), 1);
-   });
+
+  group("string calculator tests", (){
+    test('empty string test', (){
+      final val = Calculator();
+      expect(val.add(""), 0);
+    });
+    test('try single number string', () {
+      final calculator = Calculator();
+      expect(calculator.add("1"), 1);
+    });
+  });
+
 }
